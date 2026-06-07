f78129c16184f35091697c7e7e01f0f974be20dd
Simon Bosley <simon.bosley@gmail.com>
2026-06-07 13:57:58 +0100

    Adding EmailJS account details. Fixing CSS layout to work on web and mobile. Fixing form button type to NOT be submit, since we're not posting a form.

--- js/main.js
+++ js/main.js
@@ -1,16 +1,23 @@
-function sendMail(name, email, message)
+function sendMail()
 {
-    if(email == "")
+    var email = document.getElementById('email')?.value ?? "";
+    if(!email)
     {
-        console.log("No email address specified, cannot process contact info.")
+        console.log("Unable to retrieve email address.");
         return;
     }
+    
+    // Name and message are optional, so either get or set to blank.
+    var name = document.getElementById("name")?.value ?? "";
+    var message = document.getElementById("message")?.value ?? "";
 
-    const serviceID = "service_service_id";
-    const templateID = "template_template_id";
+    // Create a Data object we can use in a bit to get the current date.
+    var date = new Date();
+    var time = date.toLocaleString();
 
+    // Send message to the EmailJS API using service and template identifiers for Go Loop Audio.
     emailjs
-        .send(serviceID, templateID, { name, email, message })
+        .send("service_q7b8o7n", "template_8h8oisw", { name: name, email: email, message: message, time: time })
         .then((res) => {
             document.getElementById("name").value = "";
             document.getElementById("email").value = "";
